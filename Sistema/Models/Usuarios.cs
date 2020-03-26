ad64d584140eb28bd4d2a29e7d91c5ad7bc6b7db
Maximo Eliezer Jimeno Disla <maximojimeno@gmail.com>
2020-03-26 10:58:26 -0700

    Modelo: El Modelo Usuario es creado con sus validaciones

---
+++ Sistema/Models/Usuarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Models
+{
+   public class Usuarios
+    {
+        [Key]
+        [Range(0,100,ErrorMessage ="el ID debe ser mayor a 0 y menor a 100")]
+        public int UsuarioId { get; set; }
+        [Required(ErrorMessage ="El Nombre es Requerido ej. 'Juan Alberto'")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage ="El Apellido es Requerido ej. 'Perez Polanco'")]
+        public string Apellidos { get; set; }
+        [Required(ErrorMessage ="El Nombre de Usurio es Requerido ej. 'juanPerez, jperez'")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "La contraseña es Requeridad")]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="El Rol es requerido ej. 'Administrador, Vendedor, Operador'")]
+        public string Role { get; set; }
+
+        Usuarios()
+        {
+            UsuarioId = 0;
+            Nombres = string.Empty;
+            Apellidos = string.Empty;
+            UserName = string.Empty;
+            Password = string.Empty;
+            Role = string.Empty;
+        }
+    }
+}
