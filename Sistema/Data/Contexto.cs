adebc802083e49d675be1904ca18bbf2467e93c8
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-03-27 18:01:55 -0400

    mi primer comit

--- Sistema/Data/Contexto.cs
+++ Sistema/Data/Contexto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Sistema.Entidades;
 using Sistema.Models;
 
 namespace Sistema.Data
@@ -9,6 +10,7 @@ namespace Sistema.Data
   public class Contexto : DbContext
     {
          public  DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Clientes> clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ Sistema/Entidades/Clientes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombres = string.Empty;
+            Apellidos = string.Empty;
+            CorreoElectronico = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty; 
+        }
+    }
+}
