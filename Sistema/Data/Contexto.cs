8ad14c2e909112441370007a7991b449e4fc4b92
Maximo Eliezer Jimeno Disla <maximojimeno@gmail.com>
2020-03-27 15:17:16 -0700

    Revert "mi primer comit"
    This reverts commit adebc802083e49d675be1904ca18bbf2467e93c8.

--- Sistema/Data/Contexto.cs
+++ Sistema/Data/Contexto.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
-using Sistema.Entidades;
 using Sistema.Models;
 
 namespace Sistema.Data
@@ -10,7 +9,6 @@ namespace Sistema.Data
   public class Contexto : DbContext
     {
          public  DbSet<Usuarios> Usuarios { get; set; }
-        public DbSet<Clientes> clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
