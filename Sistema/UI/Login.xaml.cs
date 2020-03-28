7c071a997b70e302d22e9582b9ea09aee480eceb
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-03-27 20:32:56 -0400

    Revert "Revert "BLL: la BLL de Cliente fue creada""
    This reverts commit c8966c01510b51fa82584fa8f54c2c6cc8e2d797.

--- Sistema/UI/Login.xaml.cs
+++ Sistema/UI/Login.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Sistema.BLL;
-using Sistema.Models;
+using Sistema.Entidades;
 
 
 namespace Sistema.UI
