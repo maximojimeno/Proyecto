c8966c01510b51fa82584fa8f54c2c6cc8e2d797
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-03-27 20:30:21 -0400

    Revert "BLL: la BLL de Cliente fue creada"
    This reverts commit 6f87c392ee7d127eb4e3a139b320182812b1c607.

---
+++ Sistema/BLL/UsuarioBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sistema.Entidades;
+using Sistema.Data;
+
+namespace Sistema.BLL
+{
+ public  class UsuarioBLL
+    {
+
+    }
+}
